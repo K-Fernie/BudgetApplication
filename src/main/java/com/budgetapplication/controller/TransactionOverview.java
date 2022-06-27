fe95f2159bc3f46441f431f46aed29a7cc6f845a
kfernelius <kkin001@gmail.com>
2022-06-27 13:39:28 -0500

    Created BankAccount to be used for transactions, updated transaction overview to display tabular data

--- src/main/java/com/budgetapplication/controller/TransactionOverview.java
+++ src/main/java/com/budgetapplication/controller/TransactionOverview.java
@@ -1,20 +1,30 @@
 package com.budgetapplication.controller;
 
 import com.budgetapplication.controller.utils.SceneHandling;
+import com.budgetapplication.model.BankAccount;
+import com.budgetapplication.model.BucketType;
 import com.budgetapplication.model.Transaction;
 import com.budgetapplication.model.Transactions;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.InputMethodEvent;
 
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 
-public class TransactionOverview {
+public class TransactionOverview implements Initializable {
+
+    private static final ObservableList<Transaction> allTransactions = BankAccount.getAllTransactions();
 
     @FXML
     private Label accountTotalLbl;
@@ -35,7 +45,7 @@ public class TransactionOverview {
     private TextField searchTxt;
 
     @FXML
-    private TableView<?> transactionTableView;
+    private TableView<Transaction> transactionTableView;
 
     //TODO update this column with the correct type once it's created i.e. transaction type enum
     @FXML
@@ -73,5 +83,16 @@ public class TransactionOverview {
     @FXML
     void onTypeSearch(InputMethodEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //TODO fix the formatting of the Table View so the table doesn't look so trashy
+        transactionTableView.setItems(allTransactions);
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+        categoryCol.setCellValueFactory(new PropertyValueFactory<>("category"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+        amountCol.setCellValueFactory(new PropertyValueFactory<>("amount"));
     }
 }
--- src/main/java/com/budgetapplication/model/BankAccount.java
+++ src/main/java/com/budgetapplication/model/BankAccount.java
@@ -1,5 +1,53 @@
 package com.budgetapplication.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class BankAccount {
 
+    private int accountId;
+    private double accountTotal;
+    private static ObservableList<Bucket> allBuckets = FXCollections.observableArrayList();
+    private static ObservableList<Transaction> allTransactions = FXCollections.observableArrayList();
+
+    public BankAccount(int accountId, double accountTotal) {
+        this.accountId = accountId;
+        this.accountTotal = accountTotal;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public double getAccountTotal() {
+        return accountTotal;
+    }
+
+    public void setAccountTotal(double accountTotal) {
+        this.accountTotal = accountTotal;
+    }
+
+    public static ObservableList<Bucket> getAllBuckets() {
+        return allBuckets;
+    }
+
+    public void setAllBuckets(ObservableList<Bucket> allBuckets) {
+        BankAccount.allBuckets = allBuckets;
+    }
+
+    public static ObservableList<Transaction> getAllTransactions() {
+        return allTransactions;
+    }
+
+    public void setAllTransactions(ObservableList<Transaction> allTransactions) {
+        BankAccount.allTransactions = allTransactions;
+    }
+
+    public static void addTransaction(Transaction transaction){
+        allTransactions.add(transaction);
+    }
 }
